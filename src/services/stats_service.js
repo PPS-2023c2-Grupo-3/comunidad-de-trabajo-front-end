3931f111098f3f52b3c500e0a2dd93b0bb43d367
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2024-12-12 21:35:47 -0300

    agregado token a stats

--- src/services/stats_service.js
+++ src/services/stats_service.js
@@ -4,10 +4,22 @@ import { config } from "../config/config";
 
 export async function getStats() {
     try {
-        const response = await axios.get(`${config.apiUrl}/stats`);
+        const response = await axios.get(`${config.apiUrl}/stats`, {
+            headers: {
+                Authorization: `bearer ${sessionStorage.getItem("token")}`,
+            },
+        });
         return response.data;
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
+        if (error.response.status === 401) {
+            sessionStorage.clear();
+            toast.error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+            setTimeout(() => {
+                window.location.href = "/login";
+            }, 5000);
+        }
     }
-    }
-    
+}
+
